a9cdb47ddf341f2c77c278f5a05a37893f4e5d97
BraedonWatkins <watkins.braedon@gmail.com>
2022-05-19 21:04:43 -0400

    active/inactive solid brush logic

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// NOTE: Mouse States
+/*
+    1. Active: When mouse clicks inside grid
+        a. Solid: Paints solid color over any grid cell that is hovered (mouseenter)
+        b. Rainbow: Paints random color over any grid cell that is hovered (mouseenter)
+    2. Inactive (Active toggled off): Default
+*/
+let mouse = document.createElement("div");
+mouse.classList.add("active"); // should be false but for testing we will have this to true
+let brush = "solid";
+
+
 const cells = document.querySelectorAll(".cell");
 const grid = document.querySelector(".grid");
 console.log(`${cells}`);
@@ -10,21 +22,17 @@ grid.addEventListener('click', toggleBrush);
 
 
 function colorCell(e) {
-    console.log('cell');
+    if(mouse.classList.contains("active")) {
+        if(brush === "solid"){
+            e.target.style.background = "black";
+        }
+        else if(brush === "rainbow") {
+
+        }
+    }
 }
 
 function toggleBrush(e) {
-    console.log('grid');
+    mouse.classList.toggle("active");
+    console.log(`${mouse.classList}`);
 }
-
-// NOTE: Mouse States
-/*
-    1. Active: When mouse clicks inside grid
-        a. Solid: Paints solid color over any grid cell that is hovered (mouseenter)
-        b. Rainbow: Paints random color over any grid cell that is hovered (mouseenter)
-    2. Inactive (Active toggled off): Default
-*/
-
-let active = true; // should be false but for testing we will have this to true
-let brush = "solid";
-
