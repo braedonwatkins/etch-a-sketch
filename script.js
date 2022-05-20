781bd3fe234013ce51fa9ed3dc32e5a9d9beb6db
BraedonWatkins <watkins.braedon@gmail.com>
2022-05-19 20:29:39 -0400

    javascript states & cell event listener

---
+++ script.js
@@ -0,0 +1,21 @@
+const cells = document.querySelectorAll(".cell");
+const grid = document.querySelector(".grid");
+console.log(`${cells}`);
+
+
+cells.forEach((cell) => {
+    cell.addEventListener('click', () => console.log('cell'));
+});
+
+
+// NOTE: Mouse States
+/*
+    1. Active: When mouse clicks inside grid
+        a. Solid: Paints solid color over any grid cell that is hovered (mouseenter)
+        b. Rainbow: Paints random color over any grid cell that is hovered (mouseenter)
+    2. Inactive (Active toggled off): Default
+*/
+
+let active = true;
+let brush = "solid";
+
